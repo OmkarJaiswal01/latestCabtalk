5051ab742b280b90b054b98bf5bd2769821e0db2
ankitoid <97038284+ankitoid@users.noreply.github.com>
2025-03-27 15:51:16 +0530

    Update PassListController.js

--- src/controllers/PassListController.js
+++ src/controllers/PassListController.js
@@ -25,11 +25,11 @@ export const sendPassengerListToDriverByPhone = asyncHandler(async (req, res) =>
   const finalMessage = message.trim();
   try {
     const watiResponse = await axios.post(
-      `${process.env.WATI_API_URL}/api/v1/sendSessionMessage/${phoneNumber}`,
+      `https://live-mt-server.wati.io/388428/api/v1/sendSessionMessage/${phoneNumber}`,
       {},
       {  params: { messageText: finalMessage },
         headers: {
-          Authorization: `Bearer ${process.env.WATI_API_TOKEN}`,
+          Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI5MzAwNGExMi04OWZlLTQxN2MtODBiNy0zMTljMjY2ZjliNjUiLCJ1bmlxdWVfbmFtZSI6ImhhcmkudHJpcGF0aGlAZ3hpbmV0d29ya3MuY29tIiwibmFtZWlkIjoiaGFyaS50cmlwYXRoaUBneGluZXR3b3Jrcy5jb20iLCJlbWFpbCI6ImhhcmkudHJpcGF0aGlAZ3hpbmV0d29ya3MuY29tIiwiYXV0aF90aW1lIjoiMDIvMDEvMjAyNSAwODozNDo0MCIsInRlbmFudF9pZCI6IjM4ODQyOCIsImRiX25hbWUiOiJtdC1wcm9kLVRlbmFudHMiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBRE1JTklTVFJBVE9SIiwiZXhwIjoyNTM0MDIzMDA4MDAsImlzcyI6IkNsYXJlX0FJIiwiYXVkIjoiQ2xhcmVfQUkifQ.tvRl-g9OGF3kOq6FQ-PPdRtfVrr4BkfxrRKoHc7tbC0`,
           "Content-Type": "application/json",
         }, } );
     return res.status(200).json({
