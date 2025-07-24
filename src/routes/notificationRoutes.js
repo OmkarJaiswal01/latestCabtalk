12a4cf5fae578e6c03d3158e8d441b98a6f0c1ce
Omkarjaiswal01 <Omkarjaiswal799184@gmail.com>
2025-07-24 18:16:58 +0530

    new

--- src/routes/notificationRoutes.js
+++ src/routes/notificationRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { sendPickupConfirmation } from "../controllers/pickupNotificationController.js";
+import { notifyPassengerBeforePickup, sendPickupConfirmation,notifyPassengerBeforePickup } from "../controllers/pickupNotificationController.js";
 import { sendDropConfirmation } from "../controllers/dropConfirmationPassenger.js";
 import { startRideUpdatePassengerController } from "../utils/rideStartUpdatePassenger.js";
 const router = express.Router();
 router.post("/confirm-pickup", sendPickupConfirmation);
 router.post("/send-drop-con",sendDropConfirmation)
 router.post("/send_update-msg-passenger",startRideUpdatePassengerController)
+router.post("/befote_ten_minites",notifyPassengerBeforePickup)
 export default router;
