3e2ae50c90dc04f7848c1f3f8258e470b702fd03
Omkarjaiswal01 <Omkarjaiswal799184@gmail.com>
2025-08-01 17:52:43 +0530

    a2

--- src/utils/UpdateOtherPassenger.js
+++ src/utils/UpdateOtherPassenger.js
@@ -3,7 +3,7 @@ export async function UpdateOtherPassenger({
   templateName,
   broadcastName,
   parameters,
-  token
+  
 }) {
   const url = `https://live-mt-server.wati.io/388428/api/v1/sendTemplateMessage?whatsappNumber=${whatsappNumber}`;
 
@@ -11,7 +11,7 @@ export async function UpdateOtherPassenger({
     method: 'POST',
     headers: {
       'Content-Type': 'application/json-patch+json',
-      'Authorization': `Bearer ${token}`
+      'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJqdGkiOiI0YmM2MmFkNC04NTQ3LTRkYzItOTc0Ni0wNmRkMjZiODYzNmMiLCJ1bmlxdWVfbmFtZSI6Im9ta2FyLmphaXN3YWxAZ3hpbmV0d29ya3MuY29tIiwibmFtZWlkIjoib21rYXIuamFpc3dhbEBneGluZXR3b3Jrcy5jb20iLCJlbWFpbCI6Im9ta2FyLmphaXN3YWxAZ3hpbmV0d29ya3MuY29tIiwiYXV0aF90aW1lIjoiMDYvMzAvMjAyNSAwNzozNzoxNSIsInRlbmFudF9pZCI6IjM4ODQyOCIsImRiX25hbWUiOiJtdC1wcm9kLVRlbmFudHMiLCJodHRwOi8vc2NoZW1hcy5taWNyb3NvZnQuY29tL3dzLzIwMDgvMDYvaWRlbnRpdHkvY2xhaW1zL3JvbGUiOiJBRE1JTklTVFJBVE9SIiwiZXhwIjoyNTM0MDIzMDA4MDAsImlzcyI6IkNsYXJlX0FJIiwiYXVkIjoiQ2xhcmVfQUkifQ.dr6x_b4olu0EL6oJcEENiD2nMYrlQx5MWlQTJBttcqg`
     },
     body: JSON.stringify({
       template_name: templateName,
